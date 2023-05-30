3e0d6d80bfa4ae36f507eb624b76f93e40907200
Elyah <112779930+Elyah2035@users.noreply.github.com>
2023-05-30 11:27:15 +0200

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,40 +1,25 @@
-mod rustii;
-
 use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
+
+fn text_to_hex(input: &str) -> String {
+    let hex_chars: Vec<String> = input
+        .chars()
+        .map(|c| format!("{:02X}", c as u32))
+        .collect();
+
+    hex_chars.join("")
+}
 
 fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1..101);
-
     loop {
-        println!("Please input your guess.");
+        println!("Enter text to convert to hexadecimal:");
 
-        let mut guess = String::new();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read input");
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+        let text = input.trim();
+        let hex = text_to_hex(text);
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Invalid input. Please enter a number.");
-                continue;
-            }
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
+        println!("Text: {}", text);
+        println!("Hex: {}", hex);
     }
 }
