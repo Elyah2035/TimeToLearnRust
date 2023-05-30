543551bae23c82f362904083a2e17fb53a1d8c1f
Elyah <112779930+Elyah2035@users.noreply.github.com>
2023-05-30 11:35:35 +0200

    TransRights

---
+++ TransRights/main.rs
@@ -0,0 +1,123 @@
+//use colored::*;
+
+fn main() {
+    /*
+    println!("\x1B[31mThis text is red.\x1B[0m");
+    println!("\x1B[34mThis text is blue.\x1B[0m");
+    println!("\x1B[32mThis text is green.\x1B[0m");
+    println!("\x1B[33mThis text is yellow.\x1B[0m");
+
+
+        println!("\x1B[1mThis text is bold.\x1B[0m");
+        println!("\x1B[4mThis text is underlined.\x1B[0m");
+        println!("\x1B[5mThis text is blinking.\x1B[0m");
+        println!("\x1B[7mThis text is reverse.\x1B[0m");
+        println!("\x1B[8mThis text is invisible.\x1B[0m");
+
+
+    println!("\x1B[This text has a red background.\x1B[0m");
+    println!("\x1B[45mThis text has a blue background.\x1B[0m");
+
+4
+
+
+
+        println!("{} This text has a {}red{} background.", "Normal".normal().on_red(), "red".red().on_white(), "normal".normal().on_black());
+        println!("{} This text has a {}blue{} background.", "Normal".normal().on_blue(), "blue".blue().on_yellow(), "normal".normal().on_green());
+
+
+
+        let output = "Hello World";
+        println!("\\e[1;96;127m{}\\e[0m\n", output);
+
+
+        println!("{}{}{}{}{}{}{} This is the Rainbow Pride Flag!",
+                 "\x1B[31mR\x1B[0m",
+                 "\x1B[38;5;202mO\x1B[0m",
+                 "\x1B[33mY\x1B[0m",
+                 "\x1B[32mG\x1B[0m",
+                 "\x1B[34mB\x1B[0m",
+                 "\x1B[38;5;54mI\x1B[0m",
+                 "\x1B[35mV\x1B[0m");
+
+        println!("{}{}{} This is the Transgender Pride Flag!",
+                 "\x1B[38;5;111mL\x1B[0m",
+                 "\x1B[38;5;213mP\x1B[0m",
+                 "\x1B[37mW\x1B[0m");
+
+    println!("{}{}{} This is the Transgender Pride Flag!",
+             "\x1B[36mC\x1B[0m",
+             "\x1B[38;5;213mP\x1B[0m",
+             "\x1B[37mW\x1B[0m");
+*/
+    for _i in 0..20 {
+        println!("{}{}{}{}{}",
+                 "\x1B[48;5;50m   \x1B[0m",
+                 "\x1B[48;5;213m   \x1B[0m",
+                 "\x1B[48;5;15m   \x1B[0m",
+                 "\x1B[48;5;213m   \x1B[0m",
+                 "\x1B[48;5;50m   \x1B[0m");
+    }
+
+    for _i in 0..20 {
+        print!("{}",
+                 "\x1B[48;5;50m   \x1B[0m");
+    }
+    println!();
+    for _i in 0..20 {
+        print!("{}",
+               "\x1B[48;5;50m   \x1B[0m");
+    }
+
+    println!();
+    for _i in 0..20 {
+        print!("{}",
+               "\x1B[48;5;213m   \x1B[0m");
+    }
+    println!();
+    for _i in 0..20 {
+        print!("{}",
+               "\x1B[48;5;213m   \x1B[0m");
+    }
+
+
+    println!();
+    for _i in 0..20 {
+        print!("{}",
+               "\x1B[48;5;15m   \x1B[0m");
+    }
+    println!();
+    for _i in 0..20 {
+        print!("{}",
+               "\x1B[48;5;15m   \x1B[0m");
+    }
+
+
+
+    println!();
+    for _i in 0..20 {
+        print!("{}",
+               "\x1B[48;5;213m   \x1B[0m");
+    }
+    println!();
+    for _i in 0..20 {
+        print!("{}",
+               "\x1B[48;5;213m   \x1B[0m");
+    }
+    println!();
+    for _i in 0..20 {
+        print!("{}",
+               "\x1B[48;5;50m   \x1B[0m");
+    }
+    println!();
+    for _i in 0..20 {
+        print!("{}",
+               "\x1B[48;5;50m   \x1B[0m");
+    }
+
+    println!("Trans Rights!");
+
+
+
+
+}
